5f57a45a5e2f706d1e5ec58304718dd9f836c598
luya <1456887752@qq.com>
2022-06-11 09:56:22 +0800

    添加正式合约

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const dev = {
 }
 
 const prod = {
-  baseUrl: '/api/gem/',
-  contractAddress: '0xe8c2d81c82bb768ca2dc4ada1c6407732b809966',
+  baseUrl: '//api.lfggemnft.wtf/api/gem/',
+  contractAddress: '0x7350b11e511695F51D8182aA899F6E6AA59061fC',
   chainId: '0x1'
 }
 
